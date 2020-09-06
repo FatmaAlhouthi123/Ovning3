3f8261b75ba287489a9ebda6531c07bfce5e49b6
Fatma Alhouthi <fa222nx@student.lnu.se>
2020-09-06 19:56:35 +0200

    load up ovning 3

--- Ovning3/Wolfman .cs
+++ Ovning3/Wolfman .cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Ovning3 {
+    //
     class Wolfman : Wolf, IPerson {
         public string Talk() {
             return "Hi, this is wolfman!";
